62c3809261ae7137ede464cc49eea49f210035d9
xeho11 <130700886+xeho11@users.noreply.github.com>
2025-05-14 12:12:49 +0500

    Update SaveManager.lua

--- SaveManager.lua
+++ SaveManager.lua
@@ -1,7 +1,7 @@
 local httpService = game:GetService('HttpService')
 
 local SaveManager = {} do
-	SaveManager.Folder = 'MintXSettings'
+	SaveManager.Folder = 'MintX'
 	SaveManager.Ignore = {}
 	SaveManager.Parser = {
 		Toggle = {
