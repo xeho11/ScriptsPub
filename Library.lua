7dff5f13911cb76cb6b10fe9778dcc878ad0cbb8
xeho11 <130700886+xeho11@users.noreply.github.com>
2025-07-24 21:03:35 +0500

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -37,7 +37,7 @@ local Library = {
     RiskColor = Color3.fromRGB(255, 50, 50),
 
     Black = Color3.new(0, 0, 0);
-    Font = Enum.Font.Code,
+    Font = Font.new([[rbxasset://fonts/families/Montserrat.json]],Enum.FontWeight.Bold),
 
     OpenedFrames = {};
     DependencyBoxes = {};
